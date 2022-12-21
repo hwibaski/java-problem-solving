9556ee910351bf61c08106682f17df14a99cb901
ralph <hwibaski@gmail.com>
2022-12-22 02:56:45 +0900

    2022-12-22: 대소문자 변환, 문자찾기

---
+++ src/string/_string_02/mysolve/Main.java
@@ -0,0 +1,25 @@
+package string._string_02.mysolve;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(main.solution(str));
+    }
+    
+    public String solution(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isLowerCase(x)) {
+                answer += Character.toUpperCase(x);
+            } else {
+                answer += Character.toLowerCase(x);
+            }
+        }
+
+        return answer;
+    }
+}
