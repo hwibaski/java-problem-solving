a83e21911ab10ec5039965163ca4444522d415c5
ralph <hwibaski@gmail.com>
2022-12-23 00:07:18 +0900

    2022-12-23: 문장 속 가장 긴 단어 찾기

---
+++ src/string/_string_03/answer/Main.java
@@ -0,0 +1,47 @@
+package string._string_03.answer;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(main.solution(str));
+    }
+
+    public String solution(String str) {
+        String answer = "";
+        /* solution 1.
+        int m = Integer.MAX_VALUE;
+        String[] s = str.split(" ");
+        for (String x : s) {
+            int len = x.length();
+            if(len>m) {
+                m=len;
+                answer=x;
+            }
+        }
+         */
+
+        int m = Integer.MIN_VALUE, pos;
+
+        while((pos=str.indexOf(' ')) != -1) {
+            String tmp = str.substring(0, pos);
+            int len=tmp.length();
+            if(len>m) {
+                m=len;
+                answer=tmp;
+            }
+            str = str.substring(pos+1);
+        }
+
+        if (str.length() > m) {
+            answer = str;
+        }
+
+        return answer;
+    }
+
+}
