5b2547341996a4e275d04bacb3f7e9e7b5627c59
ralph <hwibaski@gmail.com>
2022-12-25 18:57:09 +0900

    2022-12-25: 문자열 뒤집기

---
+++ src/string/_string_04/answer/Main.java
@@ -0,0 +1,29 @@
+package string._string_04.answer;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for(int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+        for(String x: main.solution(n, str)) {
+
+        }
+    }
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+}
