aae6c6257eebd1fe269a0d04fe1b66f560a1cd29
ralph <hwibaski@gmail.com>
2023-01-10 00:58:32 +0900

    2023-01-10: 중복 문자 제거

---
+++ src/string/_string_06/answer/Main.java
@@ -0,0 +1,25 @@
+package string._string_06.answer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        main.solution(str);
+    }
+
+    public void solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if(str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
