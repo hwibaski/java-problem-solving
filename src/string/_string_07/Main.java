04be3a1053cfc8e4f7ee4c095b8d4b213c0daac8
ralph <hwibaski@gmail.com>
2023-05-21 15:35:36 +0900

    2023-05-21

---
+++ src/string/_string_07/Main.java
@@ -0,0 +1,26 @@
+package string._string_07;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        main.solution(str);
+    }
+
+    public void solution(String str) {
+        String answer = "";
+        String reversed = new StringBuilder(str).reverse().toString().toLowerCase();
+
+        if (reversed.equals(str.toLowerCase())) {
+            answer = "YES";
+        } else {
+            answer = "NO";
+        }
+
+        System.out.println(answer);
+    }
+}
