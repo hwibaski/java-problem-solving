bbeb2350978ebdc9785f74ed08214ba190233d64
ralph <hwibaski@gmail.com>
2023-06-03 17:43:56 +0900

    문자열 압축

--- src/inflearnbasicjava/string/string11/Main.java
+++ src/inflearnbasicjava/string/string11/Main.java
@@ -1,18 +1,3 @@
-/**
- * 문자열 압축
- * <p>
- * 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을
- * 작성하시오. 단 반복횟수가 1인 경우 생략합니다.
- * <p>
- * ▣ 입력설명 첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
- * <p>
- * ▣ 출력설명 첫 줄에 압축된 문자열을 출력한다.
- * <p>
- * ▣ 입력예제 1 KKHSSSSSSSE
- * <p>
- * ▣ 출력예제 1 K2HS7E
- */
-
 package inflearnbasicjava.string.string11;
 
 import java.util.LinkedHashMap;
@@ -25,7 +10,7 @@ public class Main {
         Main main = new Main();
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        main.solution2(s);
+        main.solution(s);
     }
 
     public void solution(String s) {
@@ -54,7 +39,7 @@ public class Main {
         System.out.println(answer);
     }
 
-    public void solution2(String s) {
+    public void teacherSolution(String s) {
         String answer = "";
         s = s + " ";
         int cnt = 1;
