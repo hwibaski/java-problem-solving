c4be8bff10db174a92d6cce2086d11e4f93a86e9
ralph <hwibaski@gmail.com>
2023-06-03 17:00:32 +0900

    패키지 구조 개선

---
+++ src/inflearnbasicjava/array/array02/Main.java
@@ -0,0 +1,30 @@
+package inflearnbasicjava.array.array02;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        main.solution(n, arr);
+    }
+
+    public void solution(int n, int[] arr) {
+        int answer = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                answer++;
+                max = arr[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}
---
+++ src/inflearnbasicjava/string/string06/my_solve/Main.java
@@ -0,0 +1,34 @@
+package inflearnbasicjava.string.string06.my_solve;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * fail temp.contains()에서 각 char를 String.valueOf()로 새로운 객체로 생성해서 넣기 때문에 예상과 같이 contains가 동작하지 않음
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        main.solution(str);
+    }
+
+    public void solution(String str) {
+        System.out.println(str);
+        List<String> temp = new ArrayList<String>();
+        char[] chars = str.toCharArray();
+
+        for (char x : chars) {
+            if (!temp.contains(x)) {
+                temp.add(String.valueOf(x));
+            }
+        }
+
+        System.out.println(temp);
+        System.out.println(String.join(",", temp));
+    }
+
+}
